aad17c7d08c04525d730a2fcdee97fdad4d10560
rajatdeeporion <rajat.thakkar@deeporion.com>
2024-11-08 11:54:44 +0530

    changes in env and and ser.js add porn no

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import server from "./index.js";
 import connectMongooseDB from "./src/config/db.config.js"; 
+const port = process.env.PORT || 4000;
+console.log(port)
 server.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
     connectMongooseDB()
   console.log("Server is listening on port 3000");
 });
