c2f43fce45903ae20945b656fdb4927c158bb9b5
rajatdeeporion <rajat.thakkar@deeporion.com>
2024-11-08 12:04:16 +0530

    changes

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
+import server from "./index.js";
 import connectMongooseDB from "./src/config/db.config.js"; 
 const port = process.env.PORT || 4000;
-import express from 'express'
-import userRoutes from './src/features/User/user.routes.js'
-const server = express();
-server.use(express.json())
-server.use('/api/user',userRoutes)
 console.log(port)
 server.get('/', (req, res) => {
   res.send('Hello, world!');
