1eaf8dcaacf05acb65cede5d784e4c575e750d7a
rajatdeeporion <rajat.thakkar@deeporion.com>
2024-11-08 12:11:37 +0530

    git scencod commit changes on config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,19 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
+
 const connectMongooseDB = async () => {
-    const dbURI = process.env.DB_Link;
-    try {
-      // Establish the connection
-      await mongoose.connect(dbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('MongoDB connected successfully');
-    } catch (err) {
-      console.error('Error connecting to MongoDB', err.message);
-      process.exit(1); // Exit process with failure
-    }
-  };
-  
-  export default connectMongooseDB;
+  const dbURI = process.env.DB_Link;
+  try {
+    // Establish the connection
+    await mongoose.connect(dbURI); // No additional options needed for MongoDB 4+
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('Error connecting to MongoDB', err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export default connectMongooseDB;
