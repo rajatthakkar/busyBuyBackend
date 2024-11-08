29ef971b66aa632f7dced5d6cac7a1370c895a14
rajatdeeporion <rajat.thakkar@deeporion.com>
2024-11-08 11:28:15 +0530

    add server / route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import server from "./index.js";
 import connectMongooseDB from "./src/config/db.config.js"; 
-
+server.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
 server.listen(3000, () => {
     connectMongooseDB()
